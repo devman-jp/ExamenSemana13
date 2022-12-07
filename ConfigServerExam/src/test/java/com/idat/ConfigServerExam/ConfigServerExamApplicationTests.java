705e8513c38778f98ce9eddb425b4e0a336cea76
JesusPlasencia <jesusplasencia2508@hotmail.com>
2022-12-06 21:22:25 -0500

    repo commit 1

---
+++ ConfigServerExam/src/test/java/com/idat/ConfigServerExam/ConfigServerExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.ConfigServerExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ EurekaServerExam/src/test/java/com/idat/EurekaServerExam/EurekaServerExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EurekaServerExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
